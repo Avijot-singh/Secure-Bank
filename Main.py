ddb9ad3b9418baf212b75440f09c0903c5b38ac5
Avijot-singh <singhavijot17@gmail.com>
2024-06-15 20:40:31 +1000

    Added Main file

---
+++ Main.py
@@ -0,0 +1,34 @@
+
+class Main:
+    def main_In():
+        
+            print("Welcome to the Banking System")
+            print("------------------------------")
+    while(True):
+            print("Are you a new or existing user?")
+            print("1.New")
+            print("2.Existing")
+            try:
+                option = int(input("Option: "))
+                if(option == 1):
+                    print([[Signp.py ]])
+                    break
+            except ValueError:
+                print("Please Enter a Number Value")
+
+    while(True):
+            print("Please Select from below options: ")
+            print("1.   Deposit")
+            print("2.   Withdraw")
+            print("3.   Check Balance")
+            print("4.   Admin Access")
+            try:
+                user = int(input("Option: "))
+                if(user == 1):
+                    print("Deposit Method")
+                    break
+            except ValueError:
+                print("Please Enter a Number Value")
+                
+
+Main.main_In()
