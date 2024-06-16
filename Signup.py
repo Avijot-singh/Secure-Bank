d3204e0e2895e68cabb4eedd5e67c967ade8afde
Avijot-singh <singhavijot17@gmail.com>
2024-06-16 22:04:37 +1000

    Signup updation

--- Signup.py
+++ Signup.py
@@ -11,8 +11,8 @@ class SignuPage:
         self.DOB = self.get_dob()
         self.Email = self.get_email()
         self.password = self.password_check()
-        self.account_number = Signup.account_number
-        Signup.account_number += 1
+        self.account_number = SignuPage.account_number
+        SignuPage.account_number += 1
 
     def get_name(self, name_type):
         while True:
@@ -48,7 +48,8 @@ class SignuPage:
         reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,10}$" # reg is the regex pattern used for validation of the password using the set conditions
         pat = re.compile(reg) #Compiles the regex pattern into a regex object for better performance.
         while True:
-            password = input("Please enter the Password you want to set:(1 UPPERCASE, 1 lowercase, 1 digit and a special character, min 6 and max 10)")
+            print("\n (1 UPPERCASE, 1 lowercase, 1 digit and a special character, min 6 and max 10)")
+            password = input("Please enter the Password you want to set: ")
             mat = re.search(pat, password) #Searches the password for a match to the compiled regex pattern pat.
             if mat:
                 print("Password Set")
@@ -61,8 +62,10 @@ class SignuPage:
         print("Hey, ", self.Fname, self.Lname, "Your Account has been set up and your Account Number is :", self.account_number, ", Next time please use your email and password to login into your account.")
 
 # Create an instance of Signup to test the input
-new_signup1 = Signup()
+new_signup1 = SignuPage()
 new_signup1.details()
+
+
 
 
 
